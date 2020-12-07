2bba814c467eac5d6613672795a1bfb5f1bcdb5e
Ane Acevedo <ane.acevedo@gmail.com>
2020-12-07 01:31:49 -0300

    Portafolio para entrega. Listo para comenzar a iterar

--- src/main.js
+++ src/main.js
@@ -1,20 +1,23 @@
 document.getElementById('root').innerHTML = `    
+<div>
+<header class="headerBar">
+    <h1 class="title">Anelisse Acevedo | Front-end Developer</h1>
+</header>
+</div>
 <div class='myName'>
 <div class='text'>
-    <h2 class='hello'>Hola Mundo!</h2>
+    <h2 class='hello'>Hola Mundo! 🌎</h2>
     <h2 class='present'>Mi nombre es Anelisse y soy desarrolladora Front-end.</h2>
 </div>
-<div class='myMoji'>
-    <img src="../img/memoji.png" alt="Anelisse" class="emoji">
-</div>
+    <img src="./img/memoji.png" alt="Anelisse" class="emoji">
 </div>
 <div class='blur-bg'>
 <div class='box-about'>
-    <h3 class='lil-title'>Sobre mi</h3>
+    <h3 class='lil-title'>Sobre mi 👩🏻‍💻</h3>
 </div>
 <div class='about-cont'>
 <p class='innertxt'>Mi nombre es Anelisse, pero todos me llaman Ane. Soy desarrolladora Front-end en Santiago de Chile.
-Me formé en como developer en Laboratoria.
+Me formé en como developer en Laboratoria 💛.
 <br/>
 Actualmente estoy buscando nuevos proyectos, experiencias y desafíos para continuar mi camino de aprendizaje.
 <br/>
@@ -23,10 +26,18 @@ Me gusta muchísimo el diseño de interfaces de usuario (UI), la tecnología y l
 Soy fotógrafa de profesión por lo que llevo años trabajando en el área visual, lo que me ha permitido entrenar muy bien mi ojo.
 <br/>Tengo muy claro hacia dónde quiero ir, mi experiencia trabajando en proyectos siempre ha sido muy grata, el trabajo en equipo y la comunicación son mis grandes habilidades. 
 <br/>
-¿Mis cosas favoritas? Los gatos, el cine, la palta y las papas fritas <3.</p>
+¿Mis cosas favoritas? Los gatos, el cine, la palta y las papas fritas 💜.</p>
+<ul class='boxes'>
+<li class='list'>
+<a href='https://drive.google.com/file/d/1Sk26qQbDQT2IZOxDYe0C80_NrCL7E41y/view?usp=sharing' class='linked'>Descarga mi CV ⬇️</a>
+</li>
+<li class='list'>
+<a href='https://www.linkedin.com/in/ane-acevedo/' class='linked'>Agrégame a tu red en LinkedIn 🌐</a>
+</li>
+</ul>
 </div>
 <div class='habilities-box'>
-<h3 class='lil-title'>Mis habilidades</h3>
+<h3 class='lil-title'>Mis habilidades 🧚🏻‍♀️</h3>
 </div>
 <div class='about-cont'>
 <p class='innertxt'>En el bootcamp de Laboratoria pude aprender a aprender a través de proyectos grupales, simulando un ambiente de trabajo por medio de metodología ágil (SCRUM).
@@ -44,6 +55,33 @@ Mis herramientras aliadas son:</p>
 <li class='list'>Figma</li>
 </ul>
 </div>
+<div class='projects'>
+<h3 class='lil-title'>Mis Proyectos 🚀</h3>
+</div>
+<div class='projects2'>
+<p class='innertxt'>Puedes revisar mis repositorios completos en mi GitHub:</p>
+<a href='https://github.com/aneacevedo'><img src='./img/gh.png' alt='github' class='mygh'></a>
+
+</div>
 </div>
 
+
+</div>
+
+<footer class='myFooter'>
+<div class='contact-txt1'>
+    <h3 class='contact-txt'>Contáctame y trabajemos juntos!</h3>
+</div>
+<div class='contact'>
+    <a class='icon-link' href='mailto:ane.acevedos@gmail.com'>
+        <img class='icons' src='./img/correo-color.png'> ane.acevedos@gmail.com
+    </a>
+    <a class='icon-link' href='https://wa.me/56965069787'>
+        <img class='icons' src='./img/wsp-color.png'> +56 9 6506 9787
+    </a>
+    <p class='icon-link'>
+        <img class='icons' src='./img/slack.png'> Ane Acevedo
+    </p>
+</div>
+</footer>
 `
